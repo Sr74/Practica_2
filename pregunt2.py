d739f4ff4d3e75c891606c0144c61af9d5fd5249
Shamil Rivera <reyiv19@gmail.com>
2018-11-20 08:33:37 -0500

    Pregunta2 Resuelta

--- pregunt2.py
+++ pregunt2.py
@@ -1,21 +1,32 @@
+def menor_valor(matriz,mayorvalor):
+    valor=mayorvalor
+    for i in matriz:
+        if(i<=valor):
+            valor=i
+    print("El menor valor es:"+str(valor))                            
+                            
+                            
                             # Entrada
 
 matriz=[]
-def menor_arreglo(matriz):
-    a=matriz[0]
-    menor = 0 
-    for i in matriz:
-        if(i<=a):
-            menor=i
-        return menor
+
 
 
                             # Proceso
 while len(matriz)<5:
     a = int(input("Ingrese un número:"))
     matriz.append(a)
-print(matriz)
-print(menor_arreglo(matriz))
+b=max(matriz)
+
+                        # Salida
+ 
+menor_valor(matriz,b)
+
+
+
+
+
+
 
 
 
--- pregunt3.py
+++ pregunt3.py
@@ -1,9 +1,10 @@
 # Entrada
-a = 3
-b = 3 
+a = int(input("Ingrese el número de filas"))
+b = int(input("Ingrese el número de columnas"))
 matriz=[]
 # Proceso
-"""
+a = int(input("Ingrese un numero"))
 for i in range(3):
     for j in range(3):
-"""       
+        matriz[i][j]= int(input("Ingrese un numero"))
+
